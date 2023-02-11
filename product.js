1bde4db3f139f2ca421930eb0ebd7d9373d54a37
Git- Rambodevv <103598800+Gitsimplon@users.noreply.github.com>
2023-02-11 16:12:27 +0100

    Update product.js

--- product.js
+++ product.js
@@ -3,5 +3,5 @@ var num1 = prompt(" Veuillez saisir la Premiere valeur:");
 var num2 = prompt("Veuillez saisir Deuxieme valeur:");
 var num3 = prompt("Veuillez saisir Troisieme valeur:");
 
-result = (num1 * num2 * num3);
+var result = (num1 * num2 * num3);
 console.log(eval(result));
