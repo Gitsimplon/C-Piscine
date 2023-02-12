4b07710e8528c387706cc1a17640f5b0f44fbd1e
Git- Rambodevv <103598800+Gitsimplon@users.noreply.github.com>
2023-02-12 09:10:00 +0100

    Update swapValues.js

--- swapValues.js
+++ swapValues.js
@@ -6,6 +6,8 @@ var swap = a;
 a = b;
 var result = "New value of a:"+ a;
 console.log(result);
-b = swap;
-result = "New value of b:"+ b;
+a = swap;
+b = a;
+
+result = "New value of a:"+ b;
 console.log(result);
